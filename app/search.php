1c449505d6eb115264fef00634c398cb6f1bce81
mbotan18 <mbotan18@gmail.com>
2024-12-03 11:13:54 +0000

    search class

---
+++ app/search.php
@@ -0,0 +1,27 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+use HasFactory;
+
+protected $fillable = [
+'name',
+'description',
+'price',
+'category_id',
+'brand_id'
+];
+
+public function category()
+{
+return $this->belongsTo(Category::class);
+}
+
+public function brand()
+{
+return $this->belongsTo(Brand::class);
+}
+}
