edaa9f19b5c35824b357686478ce0dc8c4e0d17b
Reece N <220087944@aston.ac.uk>
2024-12-03 12:42:00 +0000

    Merge branch 'main' of https://github.com/Group-5-Aston/Group-5
    j

---
+++ resources/views/components/newheader.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/logo.jpg" type="image/x-icon">
+
+    <title>
+        Pup&Purr
+    </title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+<div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section" style="background-color: #fefbe6;">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.html">
+          <span style="color: #426b1f;">
+            Pup&Purr
+          </span>
+                <img src="images/logo.jpg" alt="Logo" style="height: 60px; margin-right: 10px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class=""></span>
+            </button>
+
+            <div class="collapse navbar-collapse innerpage_navbar" id="navbarSupportedContent" style="background-color: #CFDCC0;">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropbtn" href="shop.html">Shop</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="cat.html" class="dropdown-item">cats</a></li>
+                            <li><a href="dog.html" class="dropdown-item">dogs</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="why.html">
+                            About Us
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="contact.html">Get in Touch</a>
+                    </li>
+                </ul>
+                <div class="user_option">
+                    <a href="login.html">
+                        <i class="fa fa-user" aria-hidden="true"></i>
+                        <span>
+                Login
+              </span>
+                    </a>
+                    <a href="basket.php">
+                        <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                    </a>
+                    <form class="form-inline ">
+                        <button class="btn nav_search-btn" type="submit">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+</div>
+
+{{ $slot }}
+</body>
+</html>
+
---
+++ resources/views/home/newhome.blade.php
@@ -0,0 +1,353 @@
+<x-newlayout>
+    <section class="slider_section">
+        <div class="slider_container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="detail-box">
+                                        <h1>
+                                            Welcome To Our <br>
+                                            Pet Shop
+                                        </h1>
+                                        <p>
+
+                                            Your pets deserve the very best, and we’re here to deliver! Explore our wide range of stylish pet clothing, delicious treats, and essential supplies for cats and dogs. Whether you’re pampering your furry companion or stocking up on the basics, we’ve got everything you need to keep them happy, healthy, and looking their best.                      </p>
+                                        <a href="contact.html">
+                                            Contact Us
+
+
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-5 ">
+                                    <div class="img-box">
+                                        <img src="images/mainbanner.jpg" alt="Pet Shop" class="img-fluid full-width-img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </section>
+
+    <!-- end slider section -->
+    </div>
+    <!-- end hero area -->
+
+    <!-- shop section -->
+
+    <section class="shop_section layout_padding">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    Latest Products
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="p1.html">
+                            <div class="img-box">
+
+                                <img src = "images/ring_img.jpg" alt="ring" style="width: 100%; height: auto;"></src>
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    bundle- dogfood
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £45
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    bundle- catfood
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £55
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    dog biscuits
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £10
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    cat biscuits
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £15
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    protein dog food
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £35
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    wet mixed dog food
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £10
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    wet mixed cat food
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £20
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="box">
+                        <a href="">
+                            <div class="img-box">
+
+                            </div>
+                            <div class="detail-box">
+                                <h6>
+                                    cat protein food
+                                </h6>
+                                <h6>
+                                    Price
+                                    <span>
+                    £50
+                  </span>
+                                </h6>
+                            </div>
+                            <div class="new">
+                <span>
+                  New
+                </span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- end off banner section -->
+
+            <!-- info section -->
+
+
+            <section>
+                <div class="banner">
+                    <img class="banner-img"
+                         src=" images/dog1_img.JPG"
+                         alt="banner-img">
+                    <div class="banner-content">
+                        <h4>Products for your pets you cant resist
+                        </h4>
+                        <a id="shop-now" href="shop.html">buy now</a>
+                    </div>
+                </div>\
+            </section>
+
+            //edit
+            <!-- Testimonial Section -->
+            <section class="testimonial_section">
+
+                <div class="heading_container heading_center">
+                    <h2>What Our Customers Say</h2>
+                </div>
+                <div class="swiper">
+                    <div class="swiper-wrapper">
+                        <!-- Testimonial 1 -->
+                        <div class="swiper-slide">
+                            <div class="testimonial_box">
+                                <p>
+                                    "Pup&Purr has the best pet products! My dog loves his new toy, and the delivery was super fast. Highly recommend!"
+                                </p>
+                                <h5>- Sarah L.</h5>
+                            </div>
+                        </div>
+                        <!-- Testimonial 2 -->
+                        <div class="swiper-slide">
+                            <div class="testimonial_box">
+                                <p>
+                                    "I bought a bundle of treats for my cat and she can't get enough of them. Excellent quality and customer service!"
+                                </p>
+                                <h5>- John D.</h5>
+                            </div>
+                        </div>
+                        <!-- Testimonial 3 -->
+                        <div class="swiper-slide">
+                            <div class="testimonial_box">
+                                <p>
+                                    "Amazing experience shopping here! I found everything I needed for my pets at great prices."
+                                </p>
+                                <h5>- Emily W.</h5>
+                            </div>
+                        </div>
+                        <!-- Add more testimonials as needed -->
+                    </div>
+                    <!-- Add Navigation -->
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination"></div>
+                </div>
+        </div>
+    </section>
+    <!-- Include Swiper JS and CSS -->
+
+    //edit
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+
+    <script>
+        const swiper = new Swiper('.swiper', {
+            loop: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    </script>
+    </div>
+    <section></section>
+
+    <x-newfooter>
+    </x-newfooter>
+</x-newlayout>
+
+
