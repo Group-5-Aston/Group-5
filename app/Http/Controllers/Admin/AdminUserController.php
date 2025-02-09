bf29fe04ddad1941b993a4fe158eb30ad81bfa38
Reece N <220087944@aston.ac.uk>
2025-02-09 14:47:00 +0000

    Added search bar to customers page

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
+
 
 class AdminUserController extends Controller
 {
-    public function adminCustomers() {
-        $users = User::all();
+    public function adminCustomers(Request $request) {
+        $search = $request->input('search');
+        $users = User::query();
+
+        if (!empty($search)) {
+            $users->where('name', 'like', "%$search%")
+                ->orWhere('email', 'like', "%$search%")
+                ->orWhere('usertype', 'like', "%$search%");
+        }
+
+        $users = $users->get();
+
+        if ($request->ajax()) {
+            return response()->json(['users' => $users]);
+        }
 
         return view('newpages.newadminpages.admincustomers', compact('users'));
     }
 }
+
+
--- resources/views/newpages/newadminpages/admincustomers.blade.php
+++ resources/views/newpages/newadminpages/admincustomers.blade.php
@@ -1,5 +1,8 @@
 <h1>List of Users</h1>
 
+<!-- search bar -->
+<input type="text" id="search" placeholder="Search by name, email, or usertype" autocomplete="off">
+
 <table>
     <thead>
     <tr>
@@ -13,18 +16,61 @@
         <th>Updated At</th>
     </tr>
     </thead>
-    <tbody>
-    @foreach($users as $user)
+    <tbody id="userTable">
+    @if(isset($users) && $users->count() > 0)
+        @foreach($users as $user)
+            <tr id="user{{ $user->id }}">
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->usertype }}</td>
+                <td>{{ $user->phone }}</td>
+                <td>{{ $user->address }}</td>
+                <td>{{ $user->created_at }}</td>
+                <td>{{ $user->updated_at }}</td>
+            </tr>
+        @endforeach
+    @else
         <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->usertype }}</td>
-            <td>{{ $user->phone }}</td>
-            <td>{{ $user->address }}</td>
-            <td>{{ $user->created_at }}</td>
-            <td>{{ $user->updated_at }}</td>
+            <td colspan="8">No users found.</td>
         </tr>
-    @endforeach
+    @endif
     </tbody>
 </table>
+
+<script>
+    document.getElementById('search').addEventListener('keyup', function() {
+        let searchValue = this.value;
+
+        // Make an AJAX request using Fetch API with the proper header
+        fetch(`{{ route('admin.customers') }}?search=${encodeURIComponent(searchValue)}`, {
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                let tableRows = '';
+
+                data.users.forEach(user => {
+                    tableRows += `
+                    <tr>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.usertype}</td>
+                        <td>${user.phone}</td>
+                        <td>${user.address}</td>
+                        <td>${user.created_at}</td>
+                        <td>${user.updated_at}</td>
+                    </tr>
+                `;
+                });
+
+                document.getElementById('userTable').innerHTML = tableRows;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+</script>
+
+
