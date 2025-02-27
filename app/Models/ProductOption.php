45edba7891d233efb73a882f18cc8f311d1955f7
Reece N <220087944@aston.ac.uk>
2025-02-27 10:25:32 +0000

    Added relation with basketItem and productoption

--- app/Models/ProductOption.php
+++ app/Models/ProductOption.php
@@ -13,6 +13,11 @@ class ProductOption extends Model
         return $this->belongsTo(Product::class, 'product_id');
     }
 
+    public function basketItems()
+    {
+        return $this->hasMany(BasketItem::class, 'option_id');
+    }
+
     protected $fillable = ['product_id', 'size', 'flavor','stock'];
 
     protected $primaryKey = 'option_id';
