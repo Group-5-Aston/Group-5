79286da53e049d746a71b424baff90fa6c1b1b52
Reece N <220087944@aston.ac.uk>
2025-02-27 17:11:53 +0000

    Added order and orderItem models

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'Orders';
+    protected $primaryKey = 'order_id';
+    public $timestamps = true;
+
+    protected $fillable = ['user_id', 'total', 'address', 'status', 'message'];
+    public function orderitems()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
+}
