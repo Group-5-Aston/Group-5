8b8356609b6a7ded0f3cecd3ad9cc637d7cbacae
hxk891 <158500729+hxk891@users.noreply.github.com>
2025-03-10 21:50:17 +0000

    new dog grooming and hygiene
    Signed-off-by: hxk891 <158500729+hxk891@users.noreply.github.com>

---
+++ resources/views/newpages/newdogGH.blade.php
@@ -0,0 +1,8 @@
+<x-newheader>     
+    <x-newshopshower :products="$products">
+        <h2>
+            Dog Grooming & Hygiene Products
+        </h2>
+    </x-newshopshower>
+    @include('components.newfooter')
+</x-newheader>
