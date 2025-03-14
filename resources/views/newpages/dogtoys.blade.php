e64095c8f71f16e7eeb5a19b59904364c790de8c
444fourz <nayeemahmed2004@hotmail.com>
2025-03-14 22:48:32 +0000

    fix toys for abdi

--- resources/views/newpages/dogtoys.blade.php
+++ resources/views/newpages/dogtoys.blade.php
@@ -1,7 +1,7 @@
 <x-newheader>
     <x-newshopshower :products="$products">
         <h2>
-            Dog's Clothes & Accessories
+            Dog Toys
         </h2>
     </x-newshopshower>
     @include('components.newfooter')
