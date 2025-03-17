557cf01a8803248c0de0b77317ea0a10d65742b6
mbotan18 <mbotan18@gmail.com>
2025-03-17 14:34:30 +0000

    homepage button

--- resources/views/newpages/newfilter.blade.php
+++ resources/views/newpages/newfilter.blade.php
@@ -158,13 +158,14 @@
                 @endforelse
             </div>
             
-            <div class="btn-box">
-                <a href="{{ route('fullshop') }}">
-                    Return to Main Products Page
-                </a>
-            </div>
-        @endif
-    </div>
+            <!-- Return to Home Button -->
+        <div class="text-center mt-5">
+            <a href="{{ route('home') }}" class="btn btn-success rounded-pill px-4 py-2" style="background-color: #4B7C47;">
+                Return to Homepage
+            </a>
+        </div>
+    @endif
+</div>
     
     @include('components.newfooter')
 </x-newheader>
